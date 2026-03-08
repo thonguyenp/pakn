e9111ba051b6a56bd5095776867a5956400f0b9f
thonguyenp <nphuoctho2406@gmail.com>
2026-03-08 20:37:54 +0700

    Kiểm tra trạng thái tài khoản trước khi login

--- paknProj/app/Http/Controllers/AuthController.php
+++ paknProj/app/Http/Controllers/AuthController.php
@@ -6,8 +6,9 @@ use App\Models\NguoiDung;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Log;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
@@ -42,6 +43,24 @@ class AuthController extends Controller
         return response()->json(['token' => $token], 201);
     }
 
+    // public function login(Request $request)
+    // {
+    //     $credentials = $request->validate([
+    //         'Email' => 'required|email',
+    //         'MatKhau' => 'required|string',
+    //     ]);
+
+    //     try {
+    //         if (!$token = JWTAuth::attempt(['Email' => $credentials['Email'], 'password' => $credentials['MatKhau']])) {
+    //             return response()->json(['error' => 'Thông tin đăng nhập không đúng'], 401);
+    //         }
+    //     } catch (JWTException $e) {
+    //         return response()->json(['error' => 'Không tạo được token'], 500);
+    //     }
+
+    //     return response()->json(['token' => $token]);
+    // }
+
     public function login(Request $request)
     {
         $credentials = $request->validate([
@@ -49,12 +68,15 @@ class AuthController extends Controller
             'MatKhau' => 'required|string',
         ]);
 
-        try {
-            if (!$token = JWTAuth::attempt(['Email' => $credentials['Email'], 'password' => $credentials['MatKhau']])) {
-                return response()->json(['error' => 'Thông tin đăng nhập không đúng'], 401);
-            }
-        } catch (JWTException $e) {
-            return response()->json(['error' => 'Không tạo được token'], 500);
+        if (! $token = JWTAuth::attempt(['Email' => $credentials['Email'], 'password' => $credentials['MatKhau']])) {
+            return response()->json(['error' => 'Thông tin đăng nhập không đúng'], 401);
+        }
+
+        $user = JWTAuth::setToken($token)->authenticate();
+        if ($user->TrangThai !== 1) {
+            JWTAuth::invalidate($token); // Hủy token nếu tài khoản bị khóa
+
+            return response()->json(['error' => 'Tài khoản bị khóa'], 403);
         }
 
         return response()->json(['token' => $token]);
